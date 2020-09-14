7831d2f09a78ea921c798f28c541d3f3008e579c
Volkova <study@study.com>
2020-09-14 19:22:00 +0300

    add <title>

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
+<?php
+
+
+$title = "New project php";
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Study git project</title>
+    <title><?=$title;?></title>
 </head>
 <body>
     <h1>Title</h1>
